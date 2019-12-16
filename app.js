6b9b89b3805118fa949b4bf5612a9f883f68a3b2
stepanchaparyan <stepanchaparyan@gmail.com>
2019-12-16 22:31:30 +0400

    add initial part, setup and first part of project

---
+++ app.js
@@ -0,0 +1,141 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const Event = require('./models/event');
+const User = require('./models/user');
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+
+app.use('/graphql', graphqlHttp({
+    schema: buildSchema(`
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        type User {
+            _id: ID!
+            email: String!
+            password: String
+        }
+
+        input EventInput {
+            title: String!, 
+            description: String!, 
+            price: Float!, 
+            date: String!
+        }
+
+        input UserInput {
+            email: String!, 
+            password: String!
+        }
+
+        type RootQuery {
+            events: [Event!]!
+        }
+
+        type RootMutation {
+            createEvent(eventInput: EventInput): Event 
+            createUser(userInput: UserInput): User
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () => {
+            return Event.find()
+                .then(events => {
+                    return events.map(event => {
+                        return { ...event._doc, _id: event._doc._id.toString() };
+                    });
+                })
+                .catch(err => {
+                    throw err;
+                });
+        },
+        createEvent: args => {
+            const event = new Event({
+                title: args.eventInput.title,
+                description: args.eventInput.description,
+                price: +args.eventInput.price,
+                date: new Date(args.eventInput.date),
+                creator: '5df6969c155a47361c94b913'
+            });
+            let createdEvent;
+            return event
+                .save()
+                .then(result => {
+                    createdEvent = { ...result._doc, _id: result._doc._id.toString() };
+                    return User.findById('5df6969c155a47361c94b913')
+                })
+                .then(user => {
+                    if(!user) {
+                        throw new Error('User exist already')
+                    }
+                    user.createdEvents.push(event);
+                    return user.save();
+                })
+                .then(result => {
+                    return createdEvent;
+                })
+                .catch(err => {
+                    console.log(err);
+                    throw err;
+                });
+        },
+        createUser: args => {
+            return User.findOne({ email: args.userInput.email })
+                .then(user => {
+                    if(user) {
+                        throw new Error('User exist already')
+                    }
+                    return bcrypt.hash(args.userInput.password, 12);
+                })    
+                .then(hashedPassword => {
+                    const user = new User({
+                        email: args.userInput.email,
+                        password: hashedPassword
+                    });
+                    return user.save();
+                })
+                .then(result => {
+                    return { ...result._doc, password: null, _id: result.id}
+                })
+                .catch(err => {
+                    throw err
+                });            
+        },
+    },
+    graphiql: true
+})
+);
+
+// connect to database
+mongoose.connect(`mongodb+srv://${
+    process.env.MONGO_USER}:${process.env.MONGO_PASSWORD
+    }@cluster0-xds4h.mongodb.net/${
+    process.env.MONGO_DB}?retryWrites=true&w=majority`, 
+    { useUnifiedTopology: true,
+      useNewUrlParser: true }
+    ).then(() => {
+        app.listen(PORT);
+    })
+    .catch(err => {
+        console.log(err);
+    });
+
+
