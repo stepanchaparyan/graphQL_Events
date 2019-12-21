b859873e56ded7d59c90a37327adc1f051471c5b
stepanchaparyan <stepanchaparyan@gmail.com>
2019-12-22 00:31:31 +0400

    Added auth form

--- app.js
+++ app.js
@@ -8,9 +8,19 @@ const graphQLResolvers = require('./graphql/resolvers/index');
 const isAuth = require('./middleware/is-auth');
 
 const app = express();
-const PORT = 3000;
+const PORT = 8000;
 
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authoriation');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+})
 
 app.use(isAuth);
 
